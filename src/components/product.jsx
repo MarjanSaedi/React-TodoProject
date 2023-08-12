5a338e47cdd1f824c7eda18c69f439eaef4922c1
Marjan <marjan.saedi@gmail.com>
2023-08-12 13:36:24 -0600

    change the product component to a controled component

--- src/components/product.jsx
+++ src/components/product.jsx
@@ -2,40 +2,30 @@ import './product.css'
 import React, { Component } from 'react';
 
 class Product extends Component {
-    state = {  
-        count : this.props.count ,
-        productName : this.props.ProductName
-    } 
+   
     render() { 
         return (
             <div>
-             <span className="m-2 text-info" >{this.state.productName}</span>
-             <span className="m-2 badge bg-primary">{this.state.count === 0 ? 'zero' : this.state.count}</span>
+             <span className="m-2 text-info" >{this.props.ProductName}</span>
+             <span className="m-2 badge bg-primary">{this.props.count === 0 ? 'zero' : this.props.count}</span>
              <button onClick = {this.handleIncrement} className="m-2 btn btn-sm btn-success">+</button>
              <button onClick = {this.handleDecrement} className="m-2 btn btn-sm btn-warning">-</button>
-             <button onClick={this.handleDelete} className="m-2 btn btn-sm btn-danger">delete</button>    
-            
+             <button onClick={this.handleDelete} className="m-2 btn btn-sm btn-danger">delete</button>                
          </div>
         );
     }
 
-    
     handleIncrement = () => {
-        const {count} = this.state;
-        this.setState({count : count + 1});
+        this.props.onIncrement(this.props.id);
     }
 
     handleDecrement = () => {
-        const {count} = this.state;
-        this.setState({count : count - 1});   
+        this.props.onDecrement(this.props.id)  
     }
 
     handleDelete = () => {
-        console.log(this.props.id);
         this.props.onDelete(this.props.id);
-    }
-
-    
+    }    
 }
  
 export default Product;
